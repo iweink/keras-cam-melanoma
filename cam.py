87a7842f81620af1a66251e1a8d4faaefc114a9e
Ramakrishna <ramakrishna.r@gmail.com>
2016-12-29 07:17:10 +0000

    adam converging without nan

--- cam.py
+++ cam.py
@@ -21,11 +21,35 @@ def train(dataset_path, model_path):
         tensorflow =  TensorBoard(log_dir='./logs', histogram_freq=0, write_graph=True, write_images=True)
         checkpoint_path="weights.{epoch:02d}-loss{loss:.3f}-acc{acc:.3f}-valloss{val_loss:.3f}-valacc{val_acc:.3f}.hdf5"
         checkpoint = ModelCheckpoint(checkpoint_path, monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto')
-        model.fit(X, y, nb_epoch=200, batch_size=32, shuffle=True, validation_split=0.2, verbose=1, callbacks=[checkpoint, tensorflow])
+        model.fit(X, y, nb_epoch=50, batch_size=32, shuffle=True, validation_split=0.2, verbose=1, callbacks=[checkpoint, tensorflow])
+        predictions = model.predict(np.array([X[0]]))
+        print "predictions ", predictions
 
 def launch_quiver(model_path):
   model = load_model(model_path)
   server.launch(model, port=8015,input_folder='./imgs',  classes=['pos', 'neg'])
+
+def pr(model_path, paths, output_path):
+  with K.tf.device('/gpu'):
+        K.set_session(K.tf.Session(config=K.tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)))
+        model = load_model(model_path)
+        img_paths = []
+        if(os.path.isdir(paths)):
+          img_paths =  [os.path.join(x[0],y) for x in os.walk(paths) for y in x[2]]
+        else:
+          img_paths.append(paths)
+
+        for path in img_paths:
+          print "Processing image ", path
+          original_img = cv2.imread(path, 1)
+          width, height, _ = original_img.shape
+
+          #Reshape to the network input shape (3, w, h).
+          img = np.array([np.transpose(np.float32(original_img), (2, 0, 1))])
+          
+          predictions = model.predict(img)
+          print "predictions ", predictions
+ 
 
 def visualize_class_activation_map(model_path, paths, output_path):
   with K.tf.device('/gpu'):
@@ -59,8 +83,13 @@ def visualize_class_activation_map(model_path, paths, output_path):
           for i, w in enumerate(class_weights[:, 1]):
                   cam += w * conv_outputs[i, :, :]
           print "predictions for image ", path, " is ", predictions
+          print "np.count_nonzero(np.isnan(data)) ",  np.count_nonzero(np.isnan(conv_outputs)), " is ", np.count_nonzero(~np.isnan(conv_outputs))
+          np.savetxt('convoutputs.txt', conv_outputs.flatten())
+          np.savetxt('classweights.txt', class_weights.flatten())
           cam /= np.max(cam)
+          np.savetxt('cam.txt', cam)
           cam = cv2.resize(cam, (height, width))
+          np.savetxt('cam2.txt', cam)
           heatmap = cv2.applyColorMap(np.uint8(255*cam), cv2.COLORMAP_WINTER)
           heatmap[np.where(cam < 0.2)] = 0
           img = heatmap*0.5 + original_img
@@ -87,4 +116,5 @@ if __name__ == '__main__':
         elif args.train:
                 train(args.dataset_path, args.model_path)
         else:
-                visualize_class_activation_map(args.model_path, args.image_path, args.output_path)
+                #visualize_class_activation_map(args.model_path, args.image_path, args.output_path)
+                pr(args.model_path, args.image_path, args.output_path)
